85e2e4d33472fc44b2de9371500881fc9c3cc2b5
adityabhise1111 <136826156+adityabhise1111@users.noreply.github.com>
2024-08-16 03:11:44 +0000

    conversions

--- 01_basics/3_coversion.js
+++ 01_basics/3_coversion.js
@@ -3,6 +3,8 @@ let scoree =Number(score)
 console.log(typeof score )
 
 console.log(typeof scoree )
+console.log("1"+2+3) //123
+console.log(1+2+"3") //33
 
 // "33" = 33
 // "33anv" = Nan
@@ -10,3 +12,5 @@ console.log(typeof scoree )
 // 0 = false
 //empty = false
 //valued = true
+
+console.logJ("2"===2) //checks data types also
