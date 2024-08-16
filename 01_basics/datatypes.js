229f2bda1889bfc254e550305562a38878599690
adityabhise1111 <136826156+adityabhise1111@users.noreply.github.com>
2024-08-16 06:12:33 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+//dynamic
+//Primitive
+
+// 7 types : string ,Number , Boolean , null , Undefined , Symbol ,Bigint
+const id = Symbol('456')
+const gid = Symbol('456') //id not = gid
+console.log(id === gid) //false
+
+
+// Referenve (non premitive)
+
+// Array , objects , functions
+
+const heros =["shaktiman","naagraj"]
+
+let myObj = {
+    name:"aditya",
+    age:20,
+}
+
+const myFunction = function (){
+    console.log("Namste");
+}
+
+console.log(typeof )
