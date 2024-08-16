329948f3e38f4deff34c58122995e7e73560b873
adityabhise1111 <136826156+adityabhise1111@users.noreply.github.com>
2024-08-16 02:40:34 +0000

    committing 2 files

---
+++ 01_basics/1_var.js
@@ -0,0 +1,6 @@
+const a = 35 //immutale
+let b = "aditya"//new 
+var c = "passs"// old scope problem
+//j = "jjjjhj" // not allowwed
+
+console.table([a,b,c])
---
+++ 01_basics/2_data_types.js
@@ -0,0 +1,8 @@
+"use strict" //newr version
+//alert(3+3) // usin nod js
+//number
+//bi int
+//strin
+//bool
+//null standalone
+//undefied 
