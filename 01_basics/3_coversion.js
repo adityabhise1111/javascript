cb4c9bff8d0e3b531539933e9c52caa02cac7135
adityabhise1111 <136826156+adityabhise1111@users.noreply.github.com>
2024-08-16 02:56:38 +0000

    conversions files

---
+++ 01_basics/3_coversion.js
@@ -0,0 +1,12 @@
+let score ="33"
+let scoree =Number(score)
+console.log(typeof score )
+
+console.log(typeof scoree )
+
+// "33" = 33
+// "33anv" = Nan
+// true = 1
+// 0 = false
+//empty = false
+//valued = true
