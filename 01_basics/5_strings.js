b3a8ba4510af4bb7289552979c9d6bf4126e9b50
adityabhise1111 <136826156+adityabhise1111@users.noreply.github.com>
2024-08-16 11:42:15 +0000

    string,nums

--- 01_basics/5_strings.js
+++ 01_basics/5_strings.js
@@ -1,7 +1,31 @@
 //declaration
-const GameName = new String('aditya')
+const GameName = new String('aditya-pandurang-bhise')
 const name = "adirtya"
 const repo = 50
 //console.log( name + repo + "value");
 console.log( `Hellow my name is ${name} and my repos are ${repo}`);
-console.log(GameName[0]);
+console.log(GameName[0])
+console.log(GameName.__proto__)
+console.log(GameName.length)
+console.log(GameName.toUpperCase())
+console.log(GameName.charAt(2))
+console.log(GameName.indexOf('t'))
+
+const newString = GameName.substring(0,4)
+console.log(newString);
+
+const anotherString = GameName.slice(-8,4)//only here we can use -ve indexing
+console.log(anotherString);
+
+const newStringOne = "   aditya   "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://aditya.com/aditya%20bhise"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('aditya'))
+console.log(url.includes('pratik'))
+
+console.log(GameName.split('-'))
+ 
---
+++ 01_basics/6_nums_maths.js
@@ -0,0 +1,23 @@
+const score = 400
+
+const balance = new Number(100)
+//console.log(balance)
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(1))
+
+
+const otherNumber = 23.7688
+//console.log(otherNumber.toPrecision(3))
+const hund=3400000
+//console.log(hund.toLocaleString('en-In'))
+
+//++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+console.log(Math.min(4,6,8,6))
+
+
+console.log(Math.random(-4))
