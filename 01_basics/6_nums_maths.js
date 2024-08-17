4a977f41760a5614ad433a7095b83225f527eb9a
adityabhise1111 <136826156+adityabhise1111@users.noreply.github.com>
2024-08-17 04:07:20 +0000

    Randoms

--- 01_basics/6_nums_maths.js
+++ 01_basics/6_nums_maths.js
@@ -12,12 +12,23 @@ const hund=3400000
 //console.log(hund.toLocaleString('en-In'))
 
 //++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++
-console.log(Math)
-console.log(Math.abs(-4))
-console.log(Math.round(4.6))
-console.log(Math.ceil(4.2))
-console.log(Math.floor(4.9))
-console.log(Math.min(4,6,8,6))
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4,6,8,6))
 
 
-console.log(Math.random(-4))
+
+const rand =Math.random()
+console.log(rand)
+console.log(rand * 10)
+console.log((rand * 10)+1)
+console.log(Math.floor((rand * 10)+1))
+
+
+const min =10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
