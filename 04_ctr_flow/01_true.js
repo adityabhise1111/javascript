01f4b121778e35e71838c6b86e86c07384ad4e7a
ADITYA BHISE <136826156+adityabhise1111@users.noreply.github.com>
2025-01-05 09:18:50 +0000

    fv

---
+++ 04_ctr_flow/01_true.js
@@ -0,0 +1,17 @@
+//true
+// " ","0",0," ",[],{},function(){}
+ //false
+// "",null,undefined,0,NaN,false'
+
+// if (arr.length ===0) {
+//     console.log('false');
+// }
+
+// for object
+// const obj = {};
+// if (Object.keys(obj).length === 0) {
+//     console.log('false');
+// }
+
+
+
